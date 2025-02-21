cde71544d4f79ff94abca3cc7cebf9ebc6d5351a
Keshav Kumar Thakur <keshav.thakur.58679@gmail.com>
2025-02-21 08:15:12 +0545

    First Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,7 +51,7 @@ const appRouter = createBrowserRouter([
     path: "/profile",
     element: <Profile />
   },
-  // admin ke liye yha se start hoga
+  // For admin
   {
     path:"/admin/companies",
     element: <ProtectedRoute><Companies/></ProtectedRoute>
