eab07402640e0f241b16172f8945458bc3156cb0
Keshav Kumar Thakur <keshav.thakur.58679@gmail.com>
2025-02-21 08:22:15 +0545

    second commit

--- frontend/src/components/admin/AdminJobs.jsx
+++ frontend/src/components/admin/AdminJobs.jsx
@@ -22,7 +22,7 @@ const AdminJobs = () => {
       <Navbar />
       <div className='max-w-6xl mx-auto my-10'>
         <div className='flex items-center justify-between my-5'>
-cd<Input
+          <Input
             className="w-fit"
             placeholder="Filter by name, role"
             onChange={(e) => setInput(e.target.value)}
